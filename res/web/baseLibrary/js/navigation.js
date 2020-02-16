750e63f6cc52bb34c8cb27d5b85c15c40cb6aee9
2284672637@qq.com <2284672637@qq.com>
2020-02-16 21:57:56 +0800

    升级表单的验证方式，前后端分离 将响应式抽象成了框架response class

--- res/web/baseLibrary/js/navigation.js
+++ res/web/baseLibrary/js/navigation.js
@@ -1,65 +1,40 @@
-function response(myScroll){
-	var statux;
-	var myScroll = myScroll.c;
-	function changeStatu(){
-		var offsetWid = document.documentElement.clientWidth;
-		if(offsetWid <= 640){
-			if(this.statux == 1)
-				return false;
-			this.statux = 1;
-			return true;
-		}
-		else if(offsetWid > 640 && offsetWid < 1000){
-			if(this.statux == 2)
-				return false;
-			this.statux = 2;
-			return true;
-		}else{
-			if(this.statux == 3)
-				return false;
-			this.statux = 3;
-			return true;
-		}
+class navigationResponse extends response {
+	constructor(proName,left,right,myScroll) {
+		super(proName,left,right);
+		this.myScroll = myScroll.c;
+		this.start();
 	}
-	function refresh(statux){
-		this.statux = statux || this.statux;
-		linkArr = ["","0","640","1000"];
-		link(linkArr[this.statux]);
-		setTimeout(function() {
+	refresh(statux){
+		super.refresh(statux);
+		var myScroll = this.myScroll;
+		setTimeout(function(){
 			myScroll.refresh();
 		}, 500);
 	}
-	
-	window.onresize = function(){
-		if(changeStatu())
-			refresh();
-	}
-	changeStatu();
-	refresh();
-	
-	var navigationEle = document.getElementById('navigation_ico');
-	var offsetWid640 = true;
-	var offsetWid640_1000 = true;
-	var offsetWid1000 = true;
-	navigationEle.onclick = function(){
-		var offsetWid = document.documentElement.clientWidth;
-		if(offsetWid <= 640){
-			if(offsetWid640) refresh(3);
-			else refresh(1);
-			offsetWid640 = !offsetWid640;
-		}
-		else if(offsetWid > 640 && offsetWid < 1000){
-			if(offsetWid640_1000) refresh(3);
-			else refresh(2);
-			offsetWid640_1000 = !offsetWid640_1000;
-		}else{
-			if(offsetWid1000) refresh(2);
-			else refresh(3);
-			offsetWid1000 = !offsetWid1000;
+	start(){
+		super.start();
+		var navigationEle = document.getElementById('navigation_ico');
+		navigationEle.response = this;
+		navigationEle.judge = [true,true,true]
+		navigationEle.onclick = function(){
+			var offsetWid = document.documentElement.clientWidth;
+			if(offsetWid <= 640){
+				console.log(1)
+				if(this.judge[0]) this.response.refresh(3);
+				else this.response.refresh(1);
+				this.judge[0] = !this.judge[0];
+			}
+			else if(offsetWid > 640 && offsetWid < 1000){
+				console.log(2)
+				if(this.judge[1]) this.response.refresh(3);
+				else this.response.refresh(2);
+				this.judge[1] = !this.judge[1];
+			}else{
+				console.log(3)
+				if(this.judge[2]) this.response.refresh(2);
+				else this.response.refresh(3);	
+				this.judge[2] = !this.judge[2];
+			}
 		}
 	}
-}
-function link(name){
-	var linke_response = document.getElementById('link_response');
-	linke_response.href = "../baseLibrary/css/main"+name+"px.css";
 }
