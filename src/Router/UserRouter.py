5b4dccddd65d6320401ebad7759712838a6c534b
2284672637@qq.com <2284672637@qq.com>
2020-02-18 01:58:38 +0800

    完善了部分ErrorUtil，SuccessUtil的关于用户的数据提交的返回json数据

--- src/Router/UserRouter.py
+++ src/Router/UserRouter.py
@@ -3,6 +3,9 @@ from flask_login import login_user, logout_user
 
 from src import templatePath, login_manager, MainLog
 from src.Controler.UserControler import UserControler
+from src.Util.ErrorUtil import errorUtil
+from src.Util.JsonUtil import JsonUtil
+from src.Util.SuccessUtil import successUtil
 from src.form.LoginForm import LoginForm
 from src.Model.UserModel import User
 from src.form.RegisterForm import RegisterForm
@@ -19,9 +22,12 @@ def login():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User.query.filter_by(userName=form.userName.data).first()
-        if user is not None and user.verify_password(form.password.data):
-            login_user(user)
-            return redirect(url_for('panel.index'))
+        if user is not None:
+            if user.verify_password(form.password.data):
+                login_user(user)
+                return redirect(url_for('panel.index'))
+            return errorUtil.getData('PasswordWrong')
+        return errorUtil.getData('UserNameNone')
     return render_template('login.html', form=form)
 
 @userBluePrint.route('/logout', methods=['GET', 'POST'])
@@ -35,12 +41,16 @@ def register():
         if form.validate_on_submit():
             if form.validate_userName(form.userName):
                 userControler.addUser(form)
-                return redirect(url_for('user.login'))
-            return "用户名已存在"
-        # allFiled = form.getAllFiled()
-        # for key in allFiled:
-        #     MainLog.record(MainLog.level.DEBUG,"form"+key+".errors"+allFiled[key].errors.__str__())
-        return "表单数据有误"
+                return successUtil.getData('registerSuccess')
+            return errorUtil.getData('UserNameExist')
+        errorDict = {}
+        allFiled = form.getAllFiled()
+        for key in allFiled:
+            if len(allFiled[key].errors)>0:
+                errorDict[key] = allFiled[key].errors.__str__()
+                # MainLog.record(MainLog.level.DEBUG,"form"+key+".errors"+str(len(allFiled[key].errors)))
+                # MainLog.record(MainLog.level.DEBUG,"form"+key+".errors"+allFiled[key].errors.__str__())
+        return errorUtil.getData('FormDataWrong',message=JsonUtil().dictToJson(errorDict))
     return render_template('simple_register.html', form=form)
 
 @login_manager.user_loader
