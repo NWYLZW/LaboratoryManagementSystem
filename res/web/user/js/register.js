9f541ed037bcdadf857f17fe640cf56c563ea647
Only566 <201803020134@stu.hnie.edu.cn>
2020-02-18 22:13:29 +0800

    完成lghTODO 用户名格式验证，密码强度验证，QQ账号格式验证，手机号码格式验证 如果语言不严谨或者不妥当的，你自己改一下吧！

--- res/web/user/js/register.js
+++ res/web/user/js/register.js
@@ -7,17 +7,19 @@ var options = {
 	//clearForm: true,          //成功提交后，清除所有表单元素的值
 	resetForm: true,            //成功提交后，重置所有表单元素的值
 	timeout: 3000               //限制请求的时间，当请求大于3秒后，跳出请求
-}
+};
 function validate(formData, jqForm, options){
 	//jqForm:   jQuery对象，封装了表单的元素
 	var form = jqForm[0];              //将jqForm转换为DOM对象
-	// TODO lgh 模仿我下面的检测方式，逐一检测各个值 checkValue是我下面的一个对象你可以研究一下
-	// TODO lgh 补充检测用户名是否为学号格式
 	if(!checkValue(form.userName,true,function(){
 		// 检测userName的值
-		if(this.checkValue.length!=12){
-			// 不符合标准 将序号和错误信息加到字典中执行下面的函数，并return false
+		console.log(this.checkValueX);
+		if (this.checkValueX.length!=12){
 			this.failed('1',"用户名长度错误");
+			return false;
+		}
+		if(!(/^\d{12}$/.test(this.checkValueX))){
+			this.failed('2',"用户名需全为数字");
 			return false;
 		}
 		return true;
@@ -31,18 +33,110 @@ function validate(formData, jqForm, options){
 			case '1':
 				alert(this.errorTypeDict[errorType]);
 				break;
+			case '2':
+				alert(this.errorTypeDict[errorType]);
+				break;
 			default:
 				break;
 		}
 	})) return false;
-	// TODO lgh 检测密码强度是否够高
-	// TODO lgh 检测QQ号格式是否正确
-	// TODO lgh 检测电话号码格式是否正确
-	// TODO lgh 加油，我相信你到我起来还不会写完的，哈哈哈哈
+	if(!checkValue(form.password,true,function(){
+		// 检测passWord的值
+		if (this.checkValueX.length < 6){//小于六位数
+			this.failed('1',"密码位数小于六位强度太弱");
+			return false;
+		}
+		if(/^[0-9]+$/.test(this.checkValueX)){//小于六位数或全为数字
+			this.failed('2',"密码全为数字强度太弱");
+			return false;
+		}
+		return true;
+	},function(errorType){
+		// 失败的话执行的函数
+		console.log(this.errorTypeDict[errorType]);
+		switch (errorType){
+			case '0':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '1':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '2':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			default:
+				break;
+		}
+	})) return false;
+	if(!checkValue(form.qqNum,true,function(){
+		// 检测QQ的值
+		if (this.checkValueX[0] === '0'){
+			this.failed('1',"QQ开头不为零");//开头不能为0
+			return false;
+		}
+		if(this.checkValueX.length < 5 || this.checkValueX.length > 11){
+			this.failed('1',"QQ长度不得小于5大于11");//最少5最多11
+			return false;
+		}
+		if(!(/^[0-9]+$/.test(this.checkValueX))){
+			this.failed('1',"QQ要全为数字");//全都是数字
+			return false;
+		}
+		return true;
+	},function(errorType){
+		// 失败的话执行的函数
+		console.log(this.errorTypeDict[errorType]);
+		switch (errorType){
+			case '0':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '1':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '2':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '3':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			default:
+				break;
+		}
+	})) return false;
+	if(!checkValue(form.telNum,true,function(){
+		// 检测telNum的值
+		if (!(/^\d{11}$/.test(this.checkValueX))){
+			// 不符合标准 将序号和错误信息加到字典中执行下面的函数，并return false
+			this.failed('1',"手机号为十一位数字");
+			return false;
+		}
+		if (this.checkValueX[0] === '0'){
+			// 不符合标准 将序号和错误信息加到字典中执行下面的函数，并return false
+			this.failed('1',"手机号开头不为零");
+			return false;
+		}
+		return true;
+	},function(errorType){
+		// 失败的话执行的函数
+		console.log(this.errorTypeDict[errorType]);
+		switch (errorType){
+			case '0':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '1':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			case '2':
+				alert(this.errorTypeDict[errorType]);
+				break;
+			default:
+				break;
+		}
+	})) return false;
 	return true;
-};
+}
 function checkValue(filed,checkNull,check,failedx){
-	this.checkValue = filed;
+	this.checkValueX = filed.value;
 	this.check = check;
 	this.failed = function(errorType,errorMessage){
 		this.errorTypeDict[errorType] = errorMessage;
@@ -54,7 +148,7 @@ function checkValue(filed,checkNull,check,failedx){
 		return false;
 	}
 	return this.check();
-};
+}
 
 function response(data){
 	alert("发生未知错误");
@@ -76,4 +170,4 @@ function response(data){
 		alert("发生未知错误");
 		break;
 	}
-};
+}
