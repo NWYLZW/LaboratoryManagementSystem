31862987f5451d0a69776e2b49448ef8228672da
2284672637@qq.com <2284672637@qq.com>
2020-02-20 00:26:47 +0800

    升级ajax模块 将主页按钮改成了从服务器获取数据生成

--- src/Router/MainRouter.py
+++ src/Router/MainRouter.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
-from src import templatePath
+from src import templatePath, MainLog
+from src.Util.JsonUtil import JsonUtil
 
 mainBluePrint = Blueprint(
     'main',
@@ -10,3 +12,29 @@ mainBluePrint = Blueprint(
 @mainBluePrint.route("/")
 def index():
     return render_template("hello.html")
+@mainBluePrint.route("/buttonList")
+def buttonList():
+    if not current_user.is_authenticated:
+        # MainLog.record(MainLog.level.DEBUG,"未登录")
+        return JsonUtil().dictToJson([{
+            'title': '关于',
+            'url': '#info'
+        }, {
+            'title': '登陆',
+            'url': '/user/login'
+        }, {
+            'title': '加入我们',
+            'url': '/user/register'
+        }, ])
+    else:
+        # MainLog.record(MainLog.level.DEBUG,"已登录")
+        return JsonUtil().dictToJson([{
+            'title': '关于',
+            'url': '#info'
+        }, {
+            'title': '面板',
+            'url': '/panel'
+        }, {
+            'title': '登出',
+            'url': '/user/logout'
+        }, ])
