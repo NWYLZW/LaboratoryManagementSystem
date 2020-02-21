785f856a7e6865056421a4a42d836f2344540e58
2284672637@qq.com <2284672637@qq.com>
2020-02-21 18:57:04 +0800

    添加了panel界面的进入动画

--- res/web/baseLibrary/js/ajax.js
+++ res/web/baseLibrary/js/ajax.js
@@ -4,7 +4,8 @@ class myAjax {
 		this.data = options.data || {};
 		this.method = options.method || "GET";
 		this.success = options.success || function(result) {console.log(result);};
-		this.failure = options.failure || function(e){console.log(e.status);console.log(e.status);};
+		this.failure = options.failure || function(e){console.log(e.status);};
+		this.always = options.always || function (jqXHR){console.log(jqXHR.status);};
 	}
 	ajax(){
 		$.ajax({
@@ -14,6 +15,6 @@ class myAjax {
 			success:this.success,
 			data:JSON.stringify(this.data),
 			contentType: "application/json;charset=UTF-8",
-		})
+		}).always(this.always);
 	}
 }
