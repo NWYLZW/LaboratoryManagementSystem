c25a0018fcf6a09826e614d15591bc76be5b52aa
2284672637@qq.com <2284672637@qq.com>
2020-03-16 18:31:18 +0800

    重写了info的部分样式

--- res/web/baseLibrary/js/user.js
+++ res/web/baseLibrary/js/user.js
@@ -1,6 +1,6 @@
 function getMyHeadPortrait(imgNode){
 	new ajaxGetFile({
-		url:"../my/getHeadPortrait",
+		url:"../my/getHeadPortrait?"+new Date().getTime(),
 		method:"GET",
 		success:function(result){
 			var blob = result;
@@ -13,7 +13,27 @@ function getMyHeadPortrait(imgNode){
 			imgNode.onload = function(e) {
 				window.URL.revokeObjectURL(imgNode.src);
 			};
-			imgNode.src = "../baseLibrary/img/imgLoadError.png";
+			imgNode.src = "../../baseLibrary/img/imgLoadError.png";
+		},
+		always:function(jqXHR){}
+	}).ajax();
+}
+function getBackground(imgNode){
+	new ajaxGetFile({
+		url:"../my/getBackground?"+new Date().getTime(),
+		method:"GET",
+		success:function(result){
+			var blob = result;
+			imgNode.onload = function(e) {
+				window.URL.revokeObjectURL(imgNode.src);
+			};
+			imgNode.src = window.URL.createObjectURL(blob);
+		},
+		failure:function(error){
+			imgNode.onload = function(e) {
+				window.URL.revokeObjectURL(imgNode.src);
+			};
+			imgNode.src = "../../baseLibrary/defaultBackgroundImage.png";
 		},
 		always:function(jqXHR){}
 	}).ajax();
