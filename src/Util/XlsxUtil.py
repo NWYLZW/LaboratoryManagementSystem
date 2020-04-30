006ebc7d967484fa14a1d6bbd85748dd090f7d83
yijie4188 <yijie4188@gmail.com>
2020-04-30 20:45:15 +0800

    添加XlsxUtil的任务 交由陈桢梁负责

--- src/Util/XlsxUtil.py
+++ src/Util/XlsxUtil.py
@@ -1,1 +1,15 @@
-# TODO 生成xlsx文件的帮助类
+class XlsxUtil():
+    def __init__(self):
+        pass
+    def listToXlsx(self,ListX:list):
+        # TODO 返回ListX的xlsx类型的表格文件
+        '''
+        :param ListX: 格式要求
+        [
+        ('name01','name02','name03'),
+        ('value0','value0','value0'),
+        ('value1','value1','value1'),
+        ]
+        :return:返回ListX的xlsx类型的表格文件
+        '''
+        pass
